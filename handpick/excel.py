120bc7f9d1b62d82cb16dffea5cbb67c20903e24
saturn1101 <phuongttn50.hrc@gmail.com>
2021-08-30 00:32:05 +0700

    Create excel.py

---
+++ handpick/excel.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import pandas as pd
+
+st.set_page_config(layout="wide")
+
+st.title("Handpick Files")
+input = st.file_uploader("Upload A Excel File")
+
+if input is not None:
+    try:
+        df = pd.read_excel(input)
+    except (ValueError, AssertionError):
+        st.warning("Please upload an Excel file")
